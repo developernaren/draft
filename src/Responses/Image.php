d995eb9d9d525db8c2ad2a4c3eab3cb2fcc9cdca
Naren <developer.naren@gmail.com>
2020-05-05 11:50:12 +0400

    supports watching images

--- src/Responses/Image.php
+++ src/Responses/Image.php
@@ -16,8 +16,8 @@ class Image extends AbstractResponse
     {
         parent::__construct($config, $filesystem, $path, $fullPath);
         $filename = $this->config->getBuildBaseFolder() . '/' . $path;
-        $filename = str_replace('///', '/', $filename);
-        $this->filename = str_replace('//', '/', $filename);
+
+        $this->filename = $this->removeMultipleSlashes($filename);
         $this->fullPath = $fullPath;
         $this->filesystem = $filesystem;
         $this->imageExtension = (new Str($path))->getAfterLast('.');
@@ -34,7 +34,25 @@ class Image extends AbstractResponse
                     ->then(function ($content) {
                         return new Response(200, array_merge($this->headers, $this->getMimeTypeHeader($this->imageExtension)), $content);
                     });
-            }, $this->responseNotFound());
+            }, function () {
+
+                $assetsFolder = $this->config->getAssetsBaseFolder();
+                $assetsImage = $this->removeMultipleSlashes($assetsFolder . '/' . $this->path);
+                $imageFile = $this->filesystem->file($assetsImage);
+
+                return $imageFile->exists()
+                    ->then(function () use ($imageFile) {
+
+                        $buildImagePath = $this->removeMultipleSlashes($this->config->getBuildBaseFolder() . '/' . $this->path);
+                        $imageFile->copy($this->filesystem->file($buildImagePath));
+
+                        return $imageFile->getContents()
+                            ->then(function ($content) {
+                                return new Response(200, array_merge($this->headers, $this->getMimeTypeHeader($this->imageExtension)), $content);
+                            });
+
+                    }, $this->responseNotFound());
+            });
     }
 
     public final function getMimeTypeHeader(string $imageType): array
@@ -46,5 +64,11 @@ class Image extends AbstractResponse
         ];
 
         return $imageMimeTypes[$imageType];
+    }
+
+    private function removeMultipleSlashes(string $string)
+    {
+        $string = str_replace('///', '/', $string);
+        return str_replace('//', '/', $string);
     }
 }
